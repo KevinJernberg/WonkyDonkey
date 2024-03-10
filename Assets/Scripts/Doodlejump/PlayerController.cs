56c02b385a487622fa7ca1a183532803353d6c0b
KevinJernberg <a22kevje@student.his.se>
2024-03-10 12:40:10 +0100

    doodle jumpdone

--- Assets/Scripts/Doodlejump/PlayerController.cs
+++ Assets/Scripts/Doodlejump/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
 
     private float moveX;
+
+    private float lastX = 0;
 
     void Awake()
     {
@@ -18,13 +21,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       // moveX = Input.GetAxis("Horizontal") * moveSpeed;
+        // moveX = Input.GetAxis("Horizontal") * moveSpeed;
        float pos = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
        transform.position = new Vector3(pos, transform.position.y, transform.position.z);
+       if (transform.position.y >= 115f)
+       {
+           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       }
     }
 
     private void FixedUpdate()
     {
+        if (transform.position.x > lastX)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        
+        lastX = transform.position.x; 
         Vector2 velocity = rb.velocity;
         velocity.x = moveX;
         rb.velocity = velocity;
